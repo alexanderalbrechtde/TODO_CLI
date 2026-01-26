d09e2309133f45240c1fadb933a91cab9460bc1b
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2026-01-26 16:19:48 +0100

    tasklayout

--- src/Task.php
+++ src/Task.php
@@ -4,14 +4,26 @@ namespace TaskCli;
 
 class Task
 {
-    public function __construct()
+    private string $status = 'not begun';
+    private string $created_at;
+
+
+    public function __construct(
+        private $id,
+        private $name,
+        private $description)
     {
+        $this->created_at = date("Y-m-d H:i:s");
     }
 
-    public function create()
+    public function create(): array
     {
         return [
-
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
+            "status" => $this->status,
+            "createdAt" => $this->created_at
         ];
     }
 }
