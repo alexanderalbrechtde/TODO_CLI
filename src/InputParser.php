46d2afda3f87ecb9a3c3d7c73b8127e299fa2781
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2026-01-27 00:11:49 +0100

    ToDoAdmin usage updated

--- src/InputParser.php
+++ src/InputParser.php
@@ -1,1 +1,14 @@
 <?php
+
+require_once('TodoAdministration.php');
+
+$todoAdmin = new TodoAdministration();
+
+$command = $argv[1] ?? null;
+$arg1 = $argv[2] ?? null;
+$arg2 = $argv[3] ?? null;
+
+match ($command) {
+    'add' => $todoAdmin->addTodo($arg1, $arg2),
+    'update' => $todoAdmin->updateTask(),
+};
--- src/TodoAdministration.php
+++ src/TodoAdministration.php
@@ -1,15 +1,45 @@
 <?php
 
-class TaskAdministration
+class TodoAdministration
 {
-    private array $todos;
-    private int $nextId;
+    private array $todos = [];
+    private int $nextId = 1;
     private string $todoData = "./data/todos.json";
 
 
     public function __construct()
     {
         $this->todos = $this->getTodoFile();
+        if (!empty($this->todos)) {
+            $lastTodo = end($this->todos);
+            $this->nextId = $lastTodo['id'] + 1;
+        }
+    }
+
+    public function addTodo(string $name, string $description): void
+    {
+        require_once('Todo.php');
+        //Task erstellen
+        $todo = new Todo($this->nextId, $name, $description);
+        // task im Arrayformat hinzufügen
+        $this->todos[] = $todo->create();
+        $this->saveTodoFile();
+        $this->nextId++;
+
+        echo 'ToDo successfully created';
+    }
+
+    public function saveTodoFile(): void
+    {
+        //Array in json speichern
+        $jsonContent = json_encode($this->todos, JSON_PRETTY_PRINT);
+        file_put_contents($this->todoData, $jsonContent);
+    }
+
+    public function getTodoFile(): array
+    {
+        $fileContent = file_get_contents($this->todoData);
+        return json_decode($fileContent, true) ?? [];
     }
 
     public function getAllTodos(): array
@@ -17,16 +47,17 @@ class TaskAdministration
 
     }
 
-
-    public function addTodo(string $name, string $description): void
+    public function updateTask(): void
     {
-        $task = new Task($this->nextId, $name, $description);
-        $this->todos[] = $task->create();
 
-        echo 'ToDo successfully created';
     }
 
-    public function updateWorkingOnFlag(): void
+    public function updateFlagDone(): void
+    {
+
+    }
+
+    public function updateFlagInProgress(): void
     {
 
     }
@@ -34,11 +65,6 @@ class TaskAdministration
     public function deleteTodo(): void
     {
 
-    }
-
-    public function getTodoFile()
-    {
-        //json decode json-file
     }
 
     public function getAllDoneTodos(): void
@@ -51,7 +77,7 @@ class TaskAdministration
 
     }
 
-    public function getAllNotBegunTodos: void
+    public function getAllNotBegunTodos(): void
     {
 
     }
